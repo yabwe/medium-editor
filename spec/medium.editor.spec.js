299c3d6a3b04bdda98fd061dcaba9726207429d0
Davi Ferreira <contato@daviferreira.com>
2013-06-04 00:02:27 -0300

    New tests

--- spec/medium.editor.spec.js
+++ spec/medium.editor.spec.js
@@ -1,4 +1,5 @@
-/*global MediumEditor, describe, it, expect, spyOn*/
+/*global MediumEditor, describe, it, expect, spyOn,
+         afterEach, beforeEach*/
 
 describe('Medium Editor TestCase', function () {
     'use strict';
@@ -35,24 +36,61 @@ describe('Medium Editor TestCase', function () {
             expect(editor.initElements).not.toHaveBeenCalled();
         });
 
-        it('should have a default set of options', function () {
-            var body = document.getElementsByTagName('body')[0],
-                defaultOptions = {
-                    excludedActions: [],
-                    anchorInputPlaceholder: 'Paste or type a link',
-                    diffLeft: 0,
-                    diffTop: -5,
-                    firstHeader: 'h3',
-                    secondHeader: 'h4',
-                    delay: 0
-                },
-                editor,
-                el = document.createElement('div');
-            el.className = 'editor';
-            body.appendChild(el);
-            editor = new MediumEditor('.editor');
-            body.removeChild(el);
-            expect(editor.options).toEqual(defaultOptions);
+        describe('With a valid element', function () {
+            beforeEach(function () {
+                this.body = document.getElementsByTagName('body')[0];
+                this.el = document.createElement('div');
+                this.el.className = 'editor';
+                this.body.appendChild(this.el);
+            });
+
+            afterEach(function () {
+                var elements = document.querySelectorAll('.medium-editor-toolbar'),
+                    i;
+                for (i = 0; i < elements.length; i += 1) {
+                    this.body.removeChild(elements[i]);
+                }
+                this.body.removeChild(this.el);
+            });
+
+            it('should have a default set of options', function () {
+                var defaultOptions = {
+                        excludedActions: [],
+                        anchorInputPlaceholder: 'Paste or type a link',
+                        diffLeft: 0,
+                        diffTop: -5,
+                        firstHeader: 'h3',
+                        secondHeader: 'h4',
+                        delay: 0
+                    },
+                    editor = new MediumEditor('.editor');
+                expect(editor.options).toEqual(defaultOptions);
+            });
+
+            it('should call the default initialization methods', function () {
+                spyOn(MediumEditor.prototype, 'initElements').andCallThrough();
+                spyOn(MediumEditor.prototype, 'initToolbar').andCallThrough();
+                spyOn(MediumEditor.prototype, 'bindSelect').andCallThrough();
+                spyOn(MediumEditor.prototype, 'bindButtons').andCallThrough();
+                spyOn(MediumEditor.prototype, 'bindAnchorForm').andCallThrough();
+                var editor = new MediumEditor('.editor');
+                expect(editor.id).toBe(1);
+                expect(editor.initElements).toHaveBeenCalled();
+                expect(editor.initToolbar).toHaveBeenCalled();
+                expect(editor.bindSelect).toHaveBeenCalled();
+                expect(editor.bindButtons).toHaveBeenCalled();
+                expect(editor.bindAnchorForm).toHaveBeenCalled();
+                expect(editor.initElements).toHaveBeenCalled();
+            });
+
+            it('should set the ID according to the numbers of editors instantiated', function () {
+                var editor1 = new MediumEditor('.editor'),
+                    editor2 = new MediumEditor('.editor'),
+                    editor3 = new MediumEditor('.editor');
+                expect(editor1.id).toBe(1);
+                expect(editor2.id).toBe(2);
+                expect(editor3.id).toBe(3);
+            });
         });
     });
 });
