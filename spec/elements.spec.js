ab9897bdc92c5c23215e1ce777ccb85c87d1aa11
Davi Ferreira <contato@daviferreira.com>
2014-04-28 17:09:21 +1000

    Adds disableEditing tests

--- spec/elements.spec.js
+++ spec/elements.spec.js
@@ -22,6 +22,21 @@ describe('Elements TestCase', function () {
             expect(this.el.getAttribute('contenteditable')).toEqual('true');
         });
 
+        it('should not set element contenteditable when disableEditing is true', function () {
+            var editor = new MediumEditor('.editor', {
+                disableEditing: true
+            });
+            expect(editor.elements.length).toBe(1);
+            expect(this.el.getAttribute('contenteditable')).toBeFalsy();
+        });
+
+        it('should not set element contenteditable when data-disable-editing is true', function () {
+            this.el.setAttribute('data-disable-editing', true);
+            var editor = new MediumEditor('.editor');
+            expect(editor.elements.length).toBe(1);
+            expect(this.el.getAttribute('contenteditable')).toBeFalsy();
+        });
+
         it('should set element data attr medium-element to true', function () {
             var editor = new MediumEditor('.editor');
             expect(editor.elements.length).toBe(1);
