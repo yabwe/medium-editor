1fc5f90aacaef764c310f6ea435b13db732fd248
Noris <sironx@googlemail.com>
2014-08-28 12:04:21 +0100

    add http:// if no protocol is present.

--- src/js/medium-editor.js
+++ src/js/medium-editor.js
@@ -1169,6 +1169,11 @@ if (typeof module === 'object') {
             if (this.options.checkLinkFormat) {
                 input.value = this.checkLinkFormat(input.value);
             }
+            
+            // Add http protocol if the url doesn't start with one
+            if(!/^(https?|ftps?|rtmpt?):\/\/|mailto:/.test(input.value)) {
+              input.value = "http://" + input.value;
+            }
 
             document.execCommand('createLink', false, input.value);
 
