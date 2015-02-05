dbc84014afce2cf238d7c67b49961916bdfccc16
Nate Mielnik <nathan@outlook.com>
2015-02-04 22:08:24 -0500

    Extract placeholder check into common function in placeholder.spec

--- spec/placeholder.spec.js
+++ spec/placeholder.spec.js
@@ -63,27 +63,31 @@ describe('Placeholder TestCase', function () {
         expect(editor.elements[0].className).not.toContain('medium-editor-placeholder');
     });
 
-    it('should add the default placeholder text when data-placeholder is not present', function () {
-        var editor = new MediumEditor('.editor'),
-            placeholder = window.getComputedStyle(editor.elements[0], ':after').getPropertyValue('content');
-        if (placeholder.indexOf('attr(') === 0) {
+    /*jslint regexp: true*/
+    function validatePlaceholderContent(element, expectedValue) {
+        var placeholder = window.getComputedStyle(element, ':after').getPropertyValue('content'),
+            regex = /^attr\(([^\)]+)\)$/g,
+            match = regex.exec(placeholder);
+        if (match) {
             // In firefox, getComputedStyle().getPropertyValue('content') can return attr() instead of what attr() evaluates to
-            expect(placeholder).toEqual('attr(data-placeholder)');
+            expect(match[1]).toEqual('data-placeholder');
         } else {
-            expect(placeholder).toEqual("'" + editor.options.placeholder + "'");
+            expect(placeholder).toEqual("'" + expectedValue + "'");
         }
+    }
+    /*jslint regexp: false*/
+
+    it('should add the default placeholder text when data-placeholder is not present', function () {
+        var editor = new MediumEditor('.editor');
+        validatePlaceholderContent(editor.elements[0], editor.options.placeholder);
     });
 
     it('should use the data-placeholder when it is present', function () {
-        this.el.setAttribute('data-placeholder', 'Custom placeholder');
-        var editor = new MediumEditor('.editor'),
-            placeholder = window.getComputedStyle(editor.elements[0], ':after').getPropertyValue('content');
-        if (placeholder.indexOf("attr(") === 0) {
-            // In firefox, getComputedStyle().getPropertyValue('content') can return attr() instead of what attr() evaluates to
-            expect(placeholder).toEqual("attr(data-placeholder)");
-        } else {
-            expect(placeholder).toEqual("'Custom placeholder'");
-        }
+        var editor,
+            placeholderText = 'Custom placeholder';
+        this.el.setAttribute('data-placeholder', placeholderText);
+        editor = new MediumEditor('.editor');
+        validatePlaceholderContent(editor.elements[0], placeholderText);
     });
 
     it('should not set placeholder for empty elements when disablePlaceholders is set to true', function () {
