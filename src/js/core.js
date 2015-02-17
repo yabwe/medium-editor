2aa204972a87533f31cd7c8a6fddb3b27214ab97
steoo <esseiproject@gmail.com>
2015-02-17 14:10:06 +0100

    Fixed util js, modified for mistake

--- src/js/core.js
+++ src/js/core.js
@@ -207,7 +207,6 @@ function MediumEditor(elements, options) {
                         selRange = meSelection.getSelectedParentElement(window.getSelection().getRangeAt(0)),
                         i;
     
-                    //Check also if the selection is in the editor, if so, we don't have to hide the toolbar. 
                     //This control was introduced also to avoid the toolbar to disapper when selecting from right to left and the selection ends at the beginning of the text.                                     
                     for (i = 0; i < self.elements.length; i += 1) {
                         if (mediumEditorUtil.isDescendant(self.elements[i], e.target) 
