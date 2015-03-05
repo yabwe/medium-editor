fb898084f8a88a3ff2c4ad68a3c9d77ff60a560f
Davi Ferreira <contato@daviferreira.com>
2015-03-05 23:06:03 +0100

    Add Uint8Array for IE9

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -348,6 +348,46 @@ if (!("classList" in document.createElement("_"))) {
         view.atob = Base64.decode;
     }
 
+    (function() {
+      try {
+        var a = new Uint8Array(1);
+        return; //no need
+      } catch(e) { }
+
+      function subarray(start, end) {
+        return this.slice(start, end);
+      }
+
+      function set_(array, offset) {
+        if (arguments.length < 2) offset = 0;
+        for (var i = 0, n = array.length; i < n; ++i, ++offset)
+          this[offset] = array[i] & 0xFF;
+      }
+
+      // we need typed arrays
+      function TypedArray(arg1) {
+        var result;
+        if (typeof arg1 === "number") {
+           result = new Array(arg1);
+           for (var i = 0; i < arg1; ++i)
+             result[i] = 0;
+        } else
+           result = arg1.slice(0);
+        result.subarray = subarray;
+        result.buffer = result;
+        result.byteLength = result.length;
+        result.set = set_;
+        if (typeof arg1 === "object" && arg1.buffer)
+          result.buffer = arg1.buffer;
+
+        return result;
+      }
+
+      window.Uint8Array = TypedArray;
+      window.Uint32Array = TypedArray;
+      window.Int32Array = TypedArray;
+    })();
+
 	view.URL = view.URL || view.webkitURL;
 
 	if (view.Blob && view.URL) {
