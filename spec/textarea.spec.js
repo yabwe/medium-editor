590bf407a5d64ec43e44f63fe067d25ba8376fd1
Nate Mielnik <nathan@outlook.com>
2015-04-27 17:22:25 -0400

    Textarea fixes: event handling + initialization time

--- spec/textarea.spec.js
+++ spec/textarea.spec.js
@@ -8,6 +8,12 @@ describe('Textarea TestCase', function () {
         this.el = document.createElement('textarea');
         this.el.className = 'editor';
         this.el.value = 'test content';
+        this.el.setAttribute('data-disable-toolbar', false);
+        this.el.setAttribute('data-placeholder', 'Something');
+        this.el.setAttribute('data-disable-return', false);
+        this.el.setAttribute('data-disable-double-return', false);
+        this.el.setAttribute('data-disable-preview', false);
+        this.el.setAttribute('spellcheck', true);
         document.body.appendChild(this.el);
     });
 
@@ -19,9 +25,21 @@ describe('Textarea TestCase', function () {
         }
     });
 
-    it('should accept a textarea element and "convert" it to a div', function () {
-        var editor = new MediumEditor('.editor');
+    it('should accept a textarea element and "convert" it to a div, preserving important attributes', function () {
+        var editor = new MediumEditor('.editor'),
+            textarea = this.el;
         expect(editor.elements[0].tagName.toLowerCase()).toBe('div');
+
+        var attributesToPreserve = ['data-disable-editing',
+            'data-disable-toolbar',
+            'data-placeholder',
+            'data-disable-return',
+            'data-disable-double-return',
+            'data-disable-preview',
+            'spellcheck'];
+        attributesToPreserve.forEach(function (attr) {
+            expect(editor.elements[0].getAttribute(attr)).toBe(textarea.getAttribute(attr));
+        });
     });
 
     it('should sync editor changes with the original textarea element', function () {
