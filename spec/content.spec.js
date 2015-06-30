395b59ffa68704ab8c73272529e81941928ab4a6
Marcus Paeschke <marcus@mphasize.de>
2015-06-30 11:01:37 +0200

    Update test case to reflect valid list behavior when indenting and outdenting. Make the test stricter (do not allow Webkit to insert a random `br` tag when outdenting a list)

--- spec/content.spec.js
+++ spec/content.spec.js
@@ -56,14 +56,14 @@ describe('Content TestCase', function () {
             // I was unable to find a workaround, and this works fine in a browser
             // so let's just skip skip the innerHTML check in firefox
             if (!isFirefox()) {
-                expect(this.el.innerHTML).toBe('<ol><li>lorem</li><li><ol><li>ipsum</li></ol></li></ol>');
+                expect(this.el.innerHTML).toBe('<ol><li>lorem<ol><li>ipsum</li></ol></li></ol>');
             }
         });
 
         it('with shift key, should outdent when within an <li>', function () {
-            this.el.innerHTML = '<ol><li>lorem</li><li><ol><li><span><span>ipsum</span></span></li></ol></li></ol>';
+            this.el.innerHTML = '<ol><li>lorem<ol><li><span><span>ipsum</span></span></li></ol></li></ol>';
             var editor = this.newMediumEditor('.editor'),
-                target = editor.elements[0].querySelector('ol').lastChild.firstChild.firstChild;
+                target = editor.elements[0].querySelector('ol').lastChild.lastChild.firstChild;
             spyOn(document, 'execCommand').and.callThrough();
             selectElementContents(target);
             fireEvent(target, 'keydown', {
@@ -76,7 +76,7 @@ describe('Content TestCase', function () {
             // so let's just skip skip the innerHTML check in firefox
             if (!isFirefox()) {
                 // Webkit removes the nested spans, IE does not
-                expect(this.el.innerHTML).toMatch(/<ol><li>lorem<\/li><li>(<span><span>)?ipsum(<br>|<\/span><\/span>)<\/li><\/ol>/);
+                expect(this.el.innerHTML).toMatch(/<ol><li>lorem<\/li><li>(<span><span>)?ipsum(<\/span><\/span>)?<\/li><\/ol>/);
             }
         });
     });
