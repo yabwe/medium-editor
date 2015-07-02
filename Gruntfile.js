793039797a50397a4942a14aac7b701b560cd043
Gildas <gildas.garcia@gmail.com>
2015-07-02 12:07:52 +0200

    Add Meteor.js support, http://meteor.com

--- Gruntfile.js
+++ Gruntfile.js
@@ -300,6 +300,29 @@ module.exports = function (grunt) {
         }
     };
 
+    gruntConfig.exec = {
+        'meteor-init': {
+            command: [
+                // Make sure Meteor is installed, per https://meteor.com/install.
+                // The curl'ed script is safe; takes 2 minutes to read source & check.
+                'type meteor >/dev/null 2>&1 || { curl https://install.meteor.com/ | sh; }',
+                // Meteor expects package.js to be in the root directory
+                // of the checkout, so copy it there temporarily
+                'cp meteor/package.js .'
+                ].join(';')
+        },
+        'meteor-cleanup': {
+            // remove build files and package.js
+            command: 'rm -rf .build.* versions.json package.js'
+        },
+        'meteor-test': {
+            command: 'node_modules/.bin/spacejam --mongo-url mongodb:// test-packages ./'
+        },
+        'meteor-publish': {
+            command: 'meteor publish'
+        }
+    };
+
     grunt.initConfig(gruntConfig);
 
     require('time-grunt')(grunt);
@@ -331,4 +354,9 @@ module.exports = function (grunt) {
     grunt.registerTask('patch', ['bump', 'css', 'js']);
     grunt.registerTask('minor', ['bump:minor', 'css', 'js']);
     grunt.registerTask('major', ['bump:major', 'css', 'js']);
+
+    // Meteor tasks
+    grunt.registerTask( 'meteor-test', ['exec:meteor-init', 'exec:meteor-test', 'exec:meteor-cleanup']);
+    grunt.registerTask( 'meteor-publish', ['exec:meteor-init', 'exec:meteor-publish', 'exec:meteor-cleanup']);
+    grunt.registerTask( 'meteor', ['exec:meteor-init', 'exec:meteor-test', 'exec:meteor-publish', 'exec:meteor-cleanup']);
 };
