yabwe/medium-editor

commit b1888a8b2a8ba966bf8b34dacd1d016d0fcedc84

Use Selection.select in more places

src/js/extensions/toolbar.js
@@ -359,12 +359,8 @@ var Toolbar;
                     while (adjacentNode.nodeValue.substr(offset, 1).trim().length === 0) {
                         offset = offset + 1;
                     }
-                    var newRange = this.document.createRange();
-                    newRange.setStart(adjacentNode, offset);
-                    newRange.setEnd(selectionRange.endContainer, selectionRange.endOffset);
-                    selection.removeAllRanges();
-                    selection.addRange(newRange);
-                    selectionRange = newRange;
+                    selectionRange = Selection.select(this.document, adjacentNode, offset,
+                        selectionRange.endContainer, selectionRange.offset);
                 }
             }
         },

src/js/selection.js
@@ -243,6 +243,7 @@ var Selection;
                 range.collapse(true);
             }
             doc.getSelection().addRange(range);
+            return range;
         },
 
         /**
@@ -253,17 +254,7 @@ var Selection;
          * @param  {integer}     offset  Where in the element should we jump, 0 by default
          */
         moveCursor: function (doc, node, offset) {
-            var range, sel,
-                startOffset = offset || 0;
-
-            range = doc.createRange();
-            sel = doc.getSelection();
-
-            range.setStart(node, startOffset);
-            range.collapse(true);
-
-            sel.removeAllRanges();
-            sel.addRange(range);
+            this.select(doc, node, offset);
         },
 
         getSelectionRange: function (ownerDocument) {

commit 1de26598316ddbb94db0da1bc4a5d5138b531a4e

Fix bug selection standardization bug introduced in b1888a8

src/js/extensions/toolbar.js
@@ -366,7 +366,7 @@ var Toolbar;
                         offset = offset + 1;
                     }
                     selectionRange = Selection.select(this.document, adjacentNode, offset,
-                        selectionRange.endContainer, selectionRange.offset);
+                        selectionRange.endContainer, selectionRange.endOffset);
                 }
             }
         },