f5e7756dd337acd7a7779d6d9a9d3f8a9df7401c
Anna Yafi <annayafi@gmail.com>
2016-09-19 13:27:56 +0200

    #1216 issue fix

--- dist/js/medium-editor.js
+++ dist/js/medium-editor.js
@@ -1020,6 +1020,11 @@ MediumEditor.extensions = {};
             }
         },
 
+        /*
+         * this function adds one or several classes on an a element.
+         * if el parameter is not an a, it will look for a children of el.
+         * if no a children are found, it will look for the a parent.
+         */
         addClassToAnchors: function (el, buttonClass) {
             var classes = buttonClass.split(' '),
                 i,
@@ -1029,7 +1034,15 @@ MediumEditor.extensions = {};
                     el.classList.add(classes[j]);
                 }
             } else {
-                el = el.getElementsByTagName('a');
+                var aChildren = el.getElementsByTagName('a');
+                if (aChildren.length === 0) {
+                    while (el.nodeName.toLowerCase() !== 'a') {
+                        el = el.parentNode;
+                    }
+                    el = [el];
+                } else {
+                    el = aChildren;
+                }
                 for (i = 0; i < el.length; i += 1) {
                     for (j = 0; j < classes.length; j += 1) {
                         el[i].classList.add(classes[j]);
