0e3bdb8bebeac55b1a4b0430c1524f1edda61e21
Jordan Humphreys <jordan@mailyard.net>
2017-11-16 14:21:09 -0800

    Add div to list of block level elements.

--- src/js/util.js
+++ src/js/util.js
@@ -110,7 +110,7 @@
             // elements our editor generates
             'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'pre', 'ul', 'li', 'ol',
             // all other known block elements
-            'address', 'article', 'aside', 'audio', 'canvas', 'dd', 'dl', 'dt', 'fieldset',
+            'address', 'article', 'aside', 'audio', 'canvas', 'dd', 'div', 'dl', 'dt', 'fieldset',
             'figcaption', 'figure', 'footer', 'form', 'header', 'hgroup', 'main', 'nav',
             'noscript', 'output', 'section', 'video',
             'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td'
