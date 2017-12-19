52defad7ffeb08519bf08772f8c8d4c4769730c0
Noah Chase <nchase@gmail.com>
2017-12-19 15:47:13 -0500

    use .toMatch to catch optional linebreak

--- spec/content.spec.js
+++ spec/content.spec.js
@@ -649,8 +649,7 @@ describe('Content TestCase', function () {
                 fireEvent(target, 'keydown', {
                     keyCode: MediumEditor.util.keyCode.BACKSPACE
                 });
-                this.el.innerHTML = stripLinebreak(this.el);
-                expect(this.el.innerHTML).toBe('<p>lorem ipsum</p><ul><li></li><li>lorem ipsum</li></ul>');
+                expect(this.el.innerHTML).toMatch(/^<p>lorem ipsum<\/p><ul><li>(<br>)?<\/li><li>lorem ipsum<\/li><\/ul>$/);
             });
         });
 
@@ -812,7 +811,3 @@ describe('Content TestCase', function () {
         });
     });
 });
-
-function stripLinebreak(element) {
-    return element.innerHTML.replace('<br>', '');
-}
