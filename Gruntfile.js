fc5af8416c995d4700992e06b93ac393056bfb8a
Rebecca Deprey <rdeprey@gmail.com>
2019-04-26 19:21:57 -0400

    Adding Safari versions.

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,10 +75,12 @@ module.exports = function (grunt) {
             platform: 'OS X 10.11'
         }, {
             browserName: 'safari',
-            platform: 'OS X 10.10'
+            platform: 'OS X 10.10',
+            version: '8.0'
         }, {
             browserName: 'safari',
-            platform: 'OS X 10.11'
+            platform: 'OS X 10.11',
+            version: '10.0'
         }];
 
     gruntConfig.connect = {
